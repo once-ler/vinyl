4e78310a9604cbf041f2c51e02b639bf24c84563
once-ler <htao@eztier.com>
2017-06-01 18:44:38 -0400

    Test using react-view-pager

--- src/modules/App/Presentation.js
+++ src/modules/App/Presentation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router';
 import { logout } from '../Login/Action';
@@ -11,6 +11,8 @@ import { media } from '../../components/Setting/Setting';
 import { NavRow, NavCell, LogoCell, navAction } from '../Nav';
 import Modal from '../Modal/Modal'; 
 const logoImage = require('../../../static/vinyl.svg');
+import { ViewPager, Frame, Track, View } from 'react-view-pager';
+import toClass from 'recompose/toClass';
 
 const BetterLink = styled(Link) `
   color: sandybrown;
@@ -21,7 +23,7 @@ const BetterLink = styled(Link) `
   color: ${props => props.theme.main};
 `;
 
-export default props => (
+const Presentation1 = props => (
   <Container>
     <NavRow wrap end>
       <LogoCell>
@@ -55,3 +57,70 @@ export default props => (
     <Modal></Modal>
   </Container>
 );
+
+class Presentation extends Component {
+  
+  render() {
+    return (
+      <Container>
+        <Row>
+          <LogoCell>
+          <img src={logoImage}/>
+          </LogoCell>
+          <ViewPager tag="main">
+            <Frame className="frame">
+              <Track
+                ref={c => this.frame = c}
+                viewsToShow={2}
+                infinite
+                className="frame"
+              >
+                <View>
+                <NavCell margin>
+                <BetterLink to="/">Home</BetterLink>
+                </NavCell>
+                </View>
+                <View>
+                <NavCell margin>
+                <BetterLink to="/protected">Protected</BetterLink>
+                </NavCell>
+                </View>
+                <View>
+                <NavCell margin>
+                <BetterLink to="/form">Form</BetterLink>
+                </NavCell>
+                </View>
+                <View>
+                <NavCell margin>
+                <BetterLink to="/scrollsync">ScrollSync</BetterLink>
+                </NavCell>
+                </View>
+                <View>
+                <NavCell margin>
+                <BetterLink to="/calendar">Calendar</BetterLink>
+                </NavCell>
+                </View>
+                <View>
+                <NavCell>
+                <BetterLink to="/oauth2login">Oauth2</BetterLink>
+                </NavCell>
+                </View>
+                <View>
+                <NavCell>
+                <BetterLink to="/logout" onClick={logout}>Logout</BetterLink>
+                </NavCell>
+                </View>
+              </Track>
+            </Frame>
+          </ViewPager>
+        </Row>
+        <ResponsiveRow>
+          {this.props.children}
+        </ResponsiveRow>
+        <Modal></Modal>
+      </Container>
+    );
+  }
+}
+
+export default Presentation;
