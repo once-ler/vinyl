51c4f9e1f0b43295d3d0478ba0bf24e815b1037a
once-ler <htao@eztier.com>
2017-09-28 13:12:22 -0400

    Fix missing theme provider reducer

--- src/modules/SuggestApp/reducer.js
+++ src/modules/SuggestApp/reducer.js
@@ -1,9 +1,13 @@
 /* @flow */
+import theme from '../App/ThemeAction';
+import progress from '../App/ProgressAction';
 import scrollsync from '../ScrollSync/Action';
 import suggest from '../Suggest/Action';
 import select from '../Select/Action';
 
 export default {
+  theme,
+  progress,
   scrollsync,
   suggest,
   select
