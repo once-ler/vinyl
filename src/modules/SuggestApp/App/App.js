430c27e5514e6d9ffd3b6a566f68cdd949d364e8
once-ler <htao@eztier.com>
2018-09-16 08:08:54 -0400

    Add freeze columns action

--- src/modules/SuggestApp/App/App.js
+++ src/modules/SuggestApp/App/App.js
@@ -4,11 +4,12 @@ import Container from '../../../components/Container/Container';
 import Row from '../../../components/Row/Row';
 import Cell from '../../../components/Cell/Cell';
 import connectFunc from './Connect';
-import Select from '../../Select';
+import Select from '../../Select/Select';
 import SuggestComponents from './SuggestComponents';
 import ScrollSyncComponents from './ScrollSyncComponents';
 import {suggestActions} from '../../Suggest';
 import Progress from 'react-progress-2';
+import options from './SuggestOptions';
 
 const GradientContainer = styled(Container)`
   background-image: ${p => `linear-gradient(to right, ${p.theme.secondary}, ${p.theme.main})`};
@@ -25,7 +26,7 @@ const Presentation = props => {
     <Container style={{width: '100%', position: 'relative'}}>
       <Progress.Component style={{backgroundColor: '#fefefe'}} />
       <Container style={{width: '100%', position: 'absolute', zIndex: 3, backgroundColor: 'transparent', padding: '8px 0 0 8px'}}>
-        <Select style={{maxWidth: '400px'}}/>
+        <Select style={{maxWidth: '400px'}} options={options} />
       </Container>
       <Container style={{position: 'absolute', top: 30, zIndex: 2, margin: '12px 0', backgroundColor: 'transparent', padding: '8px 0 0 8px'}}>
         <Row>{ Suggest && <Suggest /> }<Cell style={{color: props.theme.tertiary, padding: '6px 0 0 8px', maxHeight: '45px', overflow: 'hidden'}}>{props.lastInputValue}</Cell></Row>
