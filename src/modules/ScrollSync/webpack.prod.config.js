985cb50ff9a540f4b02a165b701351c73114a302
once-ler <htao@eztier.com>
2019-01-24 13:26:06 -0500

    Restore dropped file-loader

--- src/modules/ScrollSync/webpack.prod.config.js
+++ src/modules/ScrollSync/webpack.prod.config.js
@@ -78,8 +78,7 @@ var config = {
       { test: /\.css$/, loader: 'style-loader!css-loader'},
       {
         test: /\.ttf$/,
-        loader: "url-loader", // or directly file-loader
-        // include: path.resolve(__dirname, "node_modules/react-native-vector-icons"),
+        loader: "url-loader",
       },
     ],
   },
--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -104,8 +104,7 @@ module.exports = {
       },
       {
         test: /\.ttf$/,
-        loader: "url-loader", // or directly file-loader
-        include: path.resolve(__dirname, "node_modules/react-native-vector-icons"),
+        loader: "url-loader",
       },
     ]
   }
