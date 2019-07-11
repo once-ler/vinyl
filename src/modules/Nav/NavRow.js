8e89606f7178f5fb6afaa9330727a6276d4fa286
once-ler <htao@eztier.com>
2019-07-11 14:17:17 -0400

    NavRow requires z-index set

--- src/modules/Nav/NavRow.js
+++ src/modules/Nav/NavRow.js
@@ -1,15 +1,15 @@
 /* @flow */
 /* eslint max-len: 0 */
-import React from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import styled from 'styled-components';
-import Row from '../../components/Row/Row';
-import {NavIconCollapse, NavIconOpen} from '../../components/Icon/NavIcon';
-import { media } from '../../components/Setting/Setting';
-import { toggle } from './Action';
+import React from 'react'
+import { connect } from 'react-redux'
+import styled from 'styled-components'
+import Row from '../../components/Row/Row'
+import {NavIconCollapse, NavIconOpen} from '../../components/Icon/NavIcon'
+import { media } from '../../components/Setting/Setting'
+import { toggle } from './Action'
 
 const DefaultRow = styled(Row) `
+z-index: 101;
 height: 100%;
 margin: 0;
 background-repeat: no-repeat;
@@ -26,35 +26,19 @@ ${media.tablet `
   overflow: hidden;
   justify-content: flex-start;    
 `}
-`;
-
-const DefaultRow1 = styled(Row) `
-  background-color: ${props => props.theme.tertiary};
-  margin-bottom: 10px;
-  box-shadow: 0 8px 6px -6px #777;
-  overflow: hidden;
-`;
+`
 
 // dispatch() passed from parent App.
 const NavRow = props => {
-  const { children, dispatch, ...rest } = props;
+  const { children, dispatch, ...rest } = props
   return (
     <DefaultRow {...rest}>
     {children}
     { props.nav.collapse ? <NavIconCollapse onClick={e => dispatch(toggle())} /> : <NavIconOpen onClick={e => dispatch(toggle())} /> }
     </DefaultRow>
-  );
-};
-
-const NavRow1 = props => {
-  const { children, dispatch, ...rest } = props;
-  return (
-    <DefaultRow {...rest}>
-    {children}
-    </DefaultRow>
-  );
-};
+  )
+}
 
 export default connect(
   state => ({ nav: state.nav })
-)(NavRow);
+)(NavRow)
